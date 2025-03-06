2ffc4428776bd29a0d1cb97c2c615fd760dcf280
Guillaume Lauzier <55703540+guillaumelauzier@users.noreply.github.com>
2025-03-06 14:19:06 +0100

    install

--- postcss.config.js
+++ postcss.config.js
@@ -1,12 +1,17 @@
-export const plugins = {
-  'postcss-preset-env': {},
-  'postcss-import': {},
-  'tailwindcss/nesting': 'postcss-nesting',
-  'postcss-preset-env': {
-    'features': { 
-      'nesting-rules': false
-    }
+export default {
+  plugins: {
+    'postcss-preset-env': {
+      features: {
+        'nesting-rules': false,
+        'is-pseudo-class': false,
+      },
+    },
+    'postcss-import': {},
+    'tailwindcss/nesting': 'postcss-nesting',
+    'tailwindcss': {},
+    'autoprefixer': {},
+    'postcss-logical': {
+      preserve: true, // Preserve logical properties and don't convert them
+    },
   },
-  'tailwindcss': {},
-  'autoprefixer': {}
 };
